0bc05d2bc5c9d56b85f6a7e94c3af991b61fded3
Kaiyrkhan <kairolla222@gmail.com>
2023-05-08 01:12:31 +0600

    AuthShowcase removed from file;

--- my-t3-app/src/pages/user/index.tsx
+++ my-t3-app/src/pages/user/index.tsx
@@ -1,8 +1,7 @@
 import {type GetServerSideProps} from "next";
-import {signIn, signOut, useSession} from "next-auth/react";
 import {getServerAuthSession} from "~/server/auth";
 import React from "react";
-import {api} from "~/utils/api";
+import {AuthShowcase} from "../../../components/ui/user-model";
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
     const session = await getServerAuthSession(ctx);
@@ -11,8 +10,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
     };
 };
 
-export default function User() {
-    const {data: session} = useSession();
+export default function UserPage() {
     return (
         <div>
             <h1>User</h1>
@@ -21,26 +19,3 @@ export default function User() {
     )
 }
 
-export const AuthShowcase: React.FC = () => {
-    const { data: sessionData } = useSession();
-
-    const { data: secretMessage } = api.example.getSecretMessage.useQuery(
-        undefined, // no input
-        { enabled: sessionData?.user !== undefined }
-    );
-
-    return (
-        <div className="flex flex-col bg-indigo-900 items-center justify-center gap-4">
-            <p className="text-center text-2xl text-white">
-                {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-                {secretMessage && <span> - {secretMessage}</span>}
-            </p>
-            <button
-                className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-                onClick={sessionData ? () => void signOut() : () => void signIn()}
-            >
-                {sessionData ? "Sign out" : "Sign in"}
-            </button>
-        </div>
-    );
-};
