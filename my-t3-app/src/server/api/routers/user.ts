c226030771474043690869cb79b7457b88c6c8f6
Kaiyrkhan <kairolla222@gmail.com>
2023-05-07 21:35:46 +0600

    created user.ts

---
+++ my-t3-app/src/server/api/routers/user.ts
@@ -0,0 +1,12 @@
+import {createTRPCRouter, protectedProcedure,} from "~/server/api/trpc";
+import {prisma} from "~/server/db";
+
+export const userRouter = createTRPCRouter({
+     me: protectedProcedure.query(async ({ ctx }) => {
+         return await prisma.user.findUnique({
+            where: {
+                id: ctx.session.user.id,
+            },
+        });
+    }),
+});
