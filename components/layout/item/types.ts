526b4e5d7c77036848c1038227113d7b3528e476
sogeking7 <kairolla222@gmail.com>
2023-05-27 04:20:58 +0600

    feat: add getAllProducts router & show it in home page

---
+++ components/layout/item/types.ts
@@ -0,0 +1,11 @@
+export interface IItemProps {
+	data: {
+		title: string
+		description: string
+		price: number
+		quantity: number
+		rating: number
+		brand: string
+		// categoryName: string
+	}
+}
--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,10 +1,12 @@
 import { createTRPCRouter } from '~/server/api/trpc'
 import { exampleRouter } from '~/server/api/routers/example'
 import { userRouter } from './routers/user'
+import {productRouter} from "~/server/api/routers/product";
 
 export const appRouter = createTRPCRouter({
 	example: exampleRouter,
-	user: userRouter
+	user: userRouter,
+	product: productRouter
 })
 
 export type AppRouter = typeof appRouter
